f3df8b457003362e0dbd1440001700cc4a64bdcd
jackteruya <jack.teruya@gmail.com>
2023-02-02 23:36:56 -0400

    endpoint transactions

--- application/rest/transactions.py
+++ application/rest/transactions.py
@@ -1,4 +1,15 @@
+from typing import List
+
 from fastapi import APIRouter
 
+from src.transactions.domain.entity import Transactions
+from src.transactions.domain.transactions_use_case import TransactionsUseCase
 
 router: APIRouter = APIRouter(prefix="/transactions")
+
+
+@router.get("", response_model=List[Transactions])
+def get_all_transactions():
+    """Endpoint to return all pharmacies"""
+
+    return TransactionsUseCase().get_all()
