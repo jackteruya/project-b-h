e15f06120004fb442ac5d7c22ee6d6f004946ff0
jackteruya <jack.teruya@gmail.com>
2023-02-02 22:12:06 -0400

    added docstring

--- src/patients/domain/entity.py
+++ src/patients/domain/entity.py
@@ -1,10 +1,15 @@
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic.main import BaseModel
 
 
 class Patient(BaseModel):
+    """Patient Entity"""
+
     uuid: str
     first_name: str
     last_name: str
     date_of_birth: datetime
+
+    def __repr__(self):
+        return f"Patient: [uuid={self.uuid}]"
